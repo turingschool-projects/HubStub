b249096ae71ed76ee43a2e8d35bfada632a71782
worace <horace.d.williams@gmail.com>
2015-05-20 20:05:19 -0600

    bucket is in N Cali region

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -93,7 +93,7 @@ Rails.application.configure do
 
   config.paperclip_defaults = {
     storage: :s3,
-    s3_host_name: 's3.amazonaws.com',
+    s3_host_name: 's3-us-west-1.amazonaws.com',
     s3_credentials: {
       access_key_id: ENV['AWS_ACCESS_KEY_ID'],
       secret_access_key: ENV['AWS_SECRET_ACCESS_KEY']
