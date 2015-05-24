48a46e310ec01987d4d7a7939c2dd8901cd67736
Edward McCarthy <efmccarthy26@gmail.com>
2015-05-24 14:41:45 -0600

    made a column while keeping old columns. Dustin is all smug about it. I'm annoyed that he's right. But whatever. Form is fixed.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150226012450) do
+ActiveRecord::Schema.define(version: 20150524203659) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -57,6 +57,7 @@ ActiveRecord::Schema.define(version: 20150226012450) do
     t.string   "img_content_type"
     t.integer  "img_file_size"
     t.datetime "img_updated_at"
+    t.string   "image_url"
   end
 
   create_table "items", force: :cascade do |t|
