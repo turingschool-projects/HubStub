19963d05e3e491a30dbb5ea5b82bac38e99c63c3
Edward McCarthy <efmccarthy26@gmail.com>
2015-05-26 18:56:05 -0600

    found a not_found method that would be the problem

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,7 +1,7 @@
 class StaticPagesController < ApplicationController
-  def not_found
-    redirect_to root_path
-  end
+  # def not_found
+  #   redirect_to root_path
+  # end
 
   def index
     @next_events = Event.active.order(:date).first(5)
