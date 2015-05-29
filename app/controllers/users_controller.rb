36a2acd8e0ac907a7d04e9c162967ed1b56d6659
Edward McCarthy <efmccarthy26@gmail.com>
2015-05-29 10:39:15 -0600

    downcasing all emails as soon as a user tries to create profile, then checking for uniqueness.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,9 +9,10 @@ class UsersController < ApplicationController
   end
 
   def create
+    user_params[:email].downcase!
     @user = User.new(user_params)
     if @user.save
-      UserMailer.account_activation(@user).deliver_now
+      # UserMailer.account_activation(@user).deliver_now
       flash[:success] = "Please check your email to activate your account!"
       redirect_to root_url
     else
